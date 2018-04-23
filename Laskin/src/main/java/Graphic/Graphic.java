cddc192aaf5f64ea4ad2c89d1a1fb2963d26c133
tviivi <viivi.tiihonen@helsinki.fi>
2018-04-23 18:02:25 +0300

    viikon 5 deadline ehkä done

--- Laskin/src/main/java/Graphic/Graphic.java
+++ Laskin/src/main/java/Graphic/Graphic.java
@@ -13,67 +13,68 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Graphic extends Application {
+
     int thenumber;
     Calculator calculator = new Calculator();
-    
+
     @Override
     public void start(Stage window) throws Exception {
-        
+
         Canvas canvas = new Canvas(400, 400);
         BorderPane setup = new BorderPane();
         TextField current = new TextField();
         TextField summary = new TextField();
         TextField calculation = new TextField();
-        
+
         Button number1 = new Button("1");
         number1.setOnAction((event) -> {
-            thenumber = thenumber*10+1;
+            thenumber = thenumber * 10 + 1;
             current.setText("" + thenumber);
         });
         Button number2 = new Button("2");
         number2.setOnAction((event) -> {
-            thenumber = thenumber*10+2;
+            thenumber = thenumber * 10 + 2;
             current.setText("" + thenumber);
         });
         Button number3 = new Button("3");
         //number3.setOnAction((event) -> new ButtonListener(current, 3, thenumber));
         number3.setOnAction((event) -> {
-            thenumber = thenumber*10+3;
+            thenumber = thenumber * 10 + 3;
             current.setText("" + thenumber);
         });
         Button number4 = new Button("4");
         number4.setOnAction((event) -> {
-            thenumber = thenumber*10+4;
+            thenumber = thenumber * 10 + 4;
             current.setText("" + thenumber);
         });
         Button number5 = new Button("5");
         number5.setOnAction((event) -> {
-            thenumber = thenumber*10+5;
+            thenumber = thenumber * 10 + 5;
             current.setText("" + thenumber);
         });
         Button number6 = new Button("6");
         number6.setOnAction((event) -> {
-            thenumber = thenumber*10+6;
+            thenumber = thenumber * 10 + 6;
             current.setText("" + thenumber);
         });
         Button number7 = new Button("7");
         number7.setOnAction((event) -> {
-            thenumber = thenumber*10+7;
+            thenumber = thenumber * 10 + 7;
             current.setText("" + thenumber);
         });
         Button number8 = new Button("8");
         number8.setOnAction((event) -> {
-            thenumber = thenumber*10+8;
+            thenumber = thenumber * 10 + 8;
             current.setText("" + thenumber);
         });
         Button number9 = new Button("9");
         number9.setOnAction((event) -> {
-            thenumber = thenumber*10+9;
+            thenumber = thenumber * 10 + 9;
             current.setText("" + thenumber);
         });
         Button number0 = new Button("0");
         number0.setOnAction((event) -> {
-            thenumber = thenumber*10;
+            thenumber = thenumber * 10;
             current.setText("" + thenumber);
         });
         Button sum = new Button("+");
@@ -116,28 +117,28 @@ public class Graphic extends Application {
             calculation.setText(help + "^" + current.getText());
             summary.setText("= " + calculator.getMainvalue());
         });
-        
+
         HBox first = new HBox();
         HBox second = new HBox();
         HBox third = new HBox();
         HBox fourth = new HBox();
         HBox fifth = new HBox();
-        
+
         first.getChildren().addAll(sum, number0, difference, summary);
         second.getChildren().addAll(number1, number2, number3, calculation);
         third.getChildren().addAll(number4, number5, number6);
         fourth.getChildren().addAll(number7, number8, number9, current);
         fifth.getChildren().addAll(product, divide, power);
-        
+
         VBox finale = new VBox();
         finale.getChildren().addAll(fourth, third, second, first, fifth);
-        
+
         Scene buttons = new Scene(finale);
-        
+
         window.setScene(buttons);
         window.show();
     }
-    
+
     public static void main(String[] args) {
         launch(Graphic.class);
     }
